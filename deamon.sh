535d34e5b95b16900f8828c23bc6caa7ec438785
peng_ren <peng_ren@apowertec.com>
2023-12-04 14:12:33 +0800

    test deamon

---
+++ deamon.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+#这个脚本用来测试 shell脚本中，如果使用deamon 进程，deamon进程的返回值如果是非0，主进程是否能够接受到这个非0值
+
+function test_deammon()
+{
+    child_deamon &
+    ERR=$?
+    echo $ERR    
+    wait
+    ERR=$?
+    echo $ERR
+}
+
+function child_deamon()
+{
+    sleep 10
+    echo "test child"
+    return 1
+}
+
+test_deammon
